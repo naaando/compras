270964a8914f98d172bec16f2cd844426735ab6f
Fernando da Silva Sousa <wild.nando@gmail.com>
2025-01-07 04:43:02 -0300

    Adiciona funções de serialização e recuperação de usuário no user preferences

---
+++ app/lib/auth/user.g.dart
@@ -0,0 +1,23 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'user.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+User _$UserFromJson(Map<String, dynamic> json) => User(
+      id: (json['id'] as num?)?.toInt(),
+      name: json['name'] as String?,
+      email: json['email'] as String?,
+      picture: json['picture'] as String?,
+      token: json['token'] as String?,
+    );
+
+Map<String, dynamic> _$UserToJson(User instance) => <String, dynamic>{
+      'id': instance.id,
+      'name': instance.name,
+      'email': instance.email,
+      'picture': instance.picture,
+      'token': instance.token,
+    };
